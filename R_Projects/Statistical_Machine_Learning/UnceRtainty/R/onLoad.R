6c9d40feb74afe74b198150a7ca7ec924ccee769
Thomas-Burrell <treyburrell01@gmail.com>
2025-05-25 20:21:00 -0400

    add more files and projects

---
+++ R_Projects/Statistical_Machine_Learning/UnceRtainty/R/onLoad.R
@@ -0,0 +1,3 @@
+.onLoad <- function(libname, pkgname) {
+  cat("A package for helping evaluate logistic models for uncertainty and classification performance (Created by Ethan Assefa, Thomas Burrell, Tatev Gomtsyan)\n") 
+  }
